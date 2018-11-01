94d34adc899c1347790a0e13f75cd094b5f5755d
Aaron Walter <aj.walter@aol.com>
2018-10-31 19:59:06 -0500

    Mob parsing 1

--- include/proto/mob.hpp
+++ include/proto/mob.hpp
@@ -9,7 +9,9 @@ using namespace std;
 class ProtoMob : public Stream {
 public:
 	enum KeyWord {
-		BEGIN_MONSTER,
+		BAD_KWD,
+		BEGIN,
+		MONSTER,
 		NAME,
 		DESC,
 		COLOR,
@@ -19,17 +21,19 @@ public:
 		DAM,
 		SYMB,
 		RRTY,
+		END,
 	};
 
 	enum Color {
-		RED,
-		GREEN,
-		BLUE,
-		CYAN,
-		YELLOW,
-		MAGENTA,
-		WHITE,
-		BLACK
+		BAD_COLOR = 0,
+		RED     = (1 << 0),
+		GREEN   = (1 << 1),
+		BLUE    = (1 << 2),
+		CYAN    = (1 << 3),
+		YELLOW  = (1 << 4),
+		MAGENTA = (1 << 5),
+		WHITE   = (1 << 6),
+		BLACK   = (1 << 7)
 	};
 
 	static KeyWord toKeyWord(string word);
--- source/proto/mob.cpp
+++ source/proto/mob.cpp
@@ -1,11 +1,32 @@
+#include <istream>
 #include "proto/mob.hpp"
 
 ProtoMob::KeyWord ProtoMob::toKeyWord(string word) {
-	return COLOR;
+	if     (word == "BEGIN") return BEGIN;
+	else if(word == "MONSTER") return MONSTER;
+	else if(word == "NAME") return NAME;
+	else if(word == "DESC") return DESC;
+	else if(word == "COLOR") return COLOR;
+	else if(word == "SPEED") return SPEED;
+	else if(word == "ABIL") return ABIL;
+	else if(word == "HP") return HP;
+	else if(word == "DAM") return DAM;
+	else if(word == "SYMB") return SYMB;
+	else if(word == "RRTY") return RRTY;
+	else if(word == "END") return END;
+	else return BAD_KWD;
 }
 
 ProtoMob::Color ProtoMob::toColor(string color) {
-	return MAGENTA;
+	if     (color == "RED") return RED;
+	else if(color == "GREEN") return GREEN;
+	else if(color == "BLUE") return BLUE;
+	else if(color == "CYAN") return CYAN;
+	else if(color == "YELLOW") return YELLOW;
+	else if(color == "MAGENTA") return MAGENTA;
+	else if(color == "WHITE") return WHITE;
+	else if(color == "BLACK") return BLACK;
+	else return BAD_COLOR;
 }
 
 ostream &ProtoMob::dump(ostream &out) const {
@@ -13,5 +34,9 @@ ostream &ProtoMob::dump(ostream &out) const {
 }
 
 istream &ProtoMob::read(istream &in) {
+	string line;
+	in >> line;
+
+
 	return in;
 }
